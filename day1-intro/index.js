fca7d60a8487bf9cc7f09a7a2e9d71c93ef3c26f
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-06-03 13:26:20 +0530

    javascript intro

---
+++ day1-intro/index.js
@@ -0,0 +1,88 @@
+let n = 20;
+
+console.log(n)
+
+let num
+
+console.log(num)
+
+let arr= [1,2, 3, 4, 5]
+
+console.log(arr)
+
+let data = ["mit", 123, true, 55.2]
+
+data.push("red")
+
+data.push(arr)
+
+console.log(data)
+
+
+let obj = new Object
+
+obj.name = krushnal
+obj.n = 5465
+
+
+condole.log(obj)
+
+
+let prod = {
+    title: "amul milk",
+    price: 45,
+    rate:4.5
+}
+
+let prod2 = {
+    title: "butter milk",
+    price: 25,
+    rate:3.2
+}
+
+
+let prod3 = {
+    title: "curd",
+    price: 22,
+    rate:3.9
+}
+
+condole.log(prod)
+
+data.push(prod, prod2, prod3)
+
+
+console.log(data)
+
+let t = data[6]
+
+console.log(t)
+
+
+data[7].title = "amul"
+
+
+for(let i= 6; i<data.length; i++)
+{
+    let t = data [i]
+
+    document.write("<h4> title: " + t.title + "</h4>")
+    document.write("<h4> price: " + t.price + "</h4>")
+    document.write("<h4> rate: " + t.rate + "</h4>")
+}
+
+
+let.first = {
+    value : "first"
+
+}
+
+
+let second = first
+
+second.value = first
+
+console.log(first.value)
+
+console.log(second.value)
+
