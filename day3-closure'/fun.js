dd1f21854622b75431bcd694512e470572818128
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-06-05 13:05:51 +0530

    closure arrow

---
+++ day3-closure'/fun.js
@@ -0,0 +1,5 @@
+let mobile =  (num) =>  num
+
+const  sum = (num1, num2) => num1+num2
+
+console.log(sum(500, 600))
---
+++ day3-closure'/index.js
@@ -0,0 +1,14 @@
+function big()
+{
+    let num = 10
+
+    function small(){
+        return  num
+    }
+return small()
+    
+}
+
+let  val  = big()
+
+console.log(val)
