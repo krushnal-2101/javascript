f725e2d0fc36b6dc8811aaeee3f24a516894e05f
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-06-09 13:41:50 +0530

    object

---
+++ day-5 object/obj.js
@@ -0,0 +1,91 @@
+// // // let obj = new object()
+// let obj = {}
+
+// // obj.title = "amul milk"
+// // obj.price = 45
+// // obj.rate = 2.3
+
+// // console.log(obj)
+// // console.log(obj.title)
+
+// // obj.price = 54
+
+// // console.log(obj)
+
+
+// // let data  = {
+// //     org : "reliance",
+// //     found : 1985,
+// //     founder : "dhiru amabani"
+// //     // "board member" : "mukedh amabani"
+// // }
+
+
+// Object.freeze(data)
+
+// data.found = 1995;
+// data.revenue = 12000;
+
+
+// console.log(data)
+
+
+// let newobj = object.assign(obj, data)
+
+// console.log(newobj)
+
+// console.log(newobj.org)
+// console.log(newobj["board member"])
+// console.log(newobj["founder"])
+
+
+// let keys = object.key(newobj)
+// let value = object.value(newobj)
+
+// console.log(key)
+// console.log(values)
+
+
+// for(let x in newobj)
+// {
+//     console.log(x)
+//     console.log(x   + ":"  + newobj[x])
+// }
+
+// console.table(newobj)
+
+
+
+// nested
+
+// let obj1 = new object()
+
+let obj1 = {
+ title:"amul milk",
+ price:34,
+ rate:5.2
+}
+
+// let obj2 = new object()
+
+let obj2 = {}
+
+obj2.title = obj1.title
+obj2.price = obj1.price
+obj2.rate = obj1.rate
+
+obj2.title = "mahi milk"
+
+
+console.log(obj1)
+console.log(obj2)
+
+
+// let obj1 = new object()
+
+// let obj1 = {
+//      title:"amul milk",
+//      price:34,
+//      rate:5.2
+// }
+
