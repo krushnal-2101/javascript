8c0ec15c87b3e7b57afb452256c1c6937245f466
krushnal-2101 <krushnalbhatti71@gmail.com>
2025-07-02 13:12:18 +0530

    timer

---
+++ day-13 timer/style.js
@@ -0,0 +1,59 @@
+let counter = document.querySelector(".counter");
+let stop = document.querySelector(".stop");
+
+
+let alert = setTimeout(timeout, 4000)
+function timeout(){
+   
+}
+
+let count = 1
+
+let timer = setInterval(function(){
+     count++;
+    counter.textContent = count;
+}, 1000);
+
+
+stop.onclick = function(){
+    clearInterval(timer)
+    clearTimeout(alert)
+}
+
+
+
+
+let hour = document.querySelector(".hour")
+let minute = document.querySelector(".minute")
+let second = document.querySelector(".second")
+
+
+setInterval(function(){
+    let d = new Date();
+    hour.textContent = d.getHours()
+    minute.textContent = d.getMinute()
+    second.textContent = d.getSecond()
+}, 1000)
+
+
+setInterval(function()
+{
+   minute.classList += "minuteAnim";
+
+}, 6000)
+
+
+
+let timenumber = document.querySelector("timenumber")
+let aud = document.querySelector("#aud")
+let clock = document.querySelector("#clock")
+
+
+
+function startTimer(){
+     setTimeout(function(){
+            aud.play();
+            clock.classList += " shake"
+
+     }, timenumber.value*1000)
+}
