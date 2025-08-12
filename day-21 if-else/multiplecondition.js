7fe2119f761ab0261c7edb7409465e250e33e7e3
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-08-12 14:43:29 +0530

    Add files via upload

---
+++ day-21 if-else/multiplecondition.js
@@ -0,0 +1,26 @@
+let marks = 33
+
+if(marks >= 33)
+{
+    console.log("you are passed")
+}
+else if (marks >= 50)
+{
+    console.log("you have c grade")
+}
+else if (marks >= 70)
+{
+    console.log("you have b grade")
+}
+else if (marks >= 90)
+{
+    console.log("you have a grade")
+}
+else if (marks >= 100)
+{
+    console.log("you have a+ grade")
+}
+else
+{
+    console.log("you have failed")
+}
---
+++ day-21 if-else/nested.js
@@ -0,0 +1,14 @@
+let age  = 18
+
+let voterid = true
+
+if(age > 18)
+{
+   if(voterid)
+   {
+     console.log("you can vote")
+   } 
+   else{
+    console.log("you have to register vote portal")
+   }
+}
