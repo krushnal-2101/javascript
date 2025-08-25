9f987bfec1b4b3d1e20e127cc64457049ab92c17
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2025-08-25 13:25:52 +0530

    Add files via upload

---
+++ day-29 string/string-method.js
@@ -0,0 +1,38 @@
+
+let text = "hellow krushnal"
+
+
+console.log("finding match in string:" + text.match("kru"))
+
+
+console.log("not finding match in string:" + text.match("abc"))
+
+
+console.log("reapiting the string: " + text.repeat(3))
+
+
+console.log("replace in string:" + text.replace("hellow" , "hiiii"))
+
+
+console.log("replacing all in string:" + text.replaceAll("k", "K"))
+
+
+console.log("searching l in element:" + text.search("l"))
+
+
+console.log("indexOf op with starting index =>" + text.indexOf("a", "5"))
+
+
+
+console.log("text example:", text)
+
+
+console.log("slice op:", text.slice(3))
+
+console.log("slice op:", text.slice(2, 5))
+
+console.log("slice OP", text.slice(2, 7));
+
+console.log("slice OP :", text.slice(-1));
+
+
